4bc696927c98fc06fbb61d09922dbaf80a2ee42c
jyj9111 <mrjang9111@gmail.com>
2023-05-04 17:30:55 +0900

    feat: first commit

---
+++ Algorithm/Algorithm/src/main/java/org/example/baekjoon/bk10871.java
@@ -0,0 +1,10 @@
+package org.example.baekjoon;
+
+import java.util.Scanner;
+
+public class bk10871 {
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+    }
+}
