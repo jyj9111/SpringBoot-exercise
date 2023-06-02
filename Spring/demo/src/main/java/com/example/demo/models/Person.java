4f80abf288979f788be0bd4fb1342be3b65dccac
jyj9111 <mrjang9111@gmail.com>
2023-06-02 14:31:45 +0900

    feat: REST로 JPA 데이터에 엑세스

--- Spring/demo/src/main/java/com/example/demo/models/Person.java
+++ Spring/demo/src/main/java/com/example/demo/models/Person.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Customer {
+public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,29 +15,19 @@ public class Customer {
     private String firstName;
     private String lastName;
 
-    protected Customer() {
-    }
-
-    public Customer(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Customer[id=%d, fristName=%s lastName=%s]",
-                id, firstName, lastName);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
