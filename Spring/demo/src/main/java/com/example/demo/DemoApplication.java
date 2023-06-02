f49b97f43e1d099f32a5ca93cfebd3a9e0933534
jyj9111 <mrjang9111@gmail.com>
2023-06-02 14:11:45 +0900

    feat: Spring jpa sqlite에 데이터 저장 - findAll, findById, findByLastName

--- Spring/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.models.Customer;
+import com.example.demo.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -17,5 +24,29 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Function hello()";
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return args -> {
+			repository.save(new Customer("leo", "aa"));
+			repository.save(new Customer("yui", "bb"));
+			repository.save(new Customer("jun", "cc"));
+			repository.save(new Customer("leo2", "aa"));
+
+			log.info("findAll----------------------------");
+			for(Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			log.info("findById---------------------------");
+			Customer customer = repository.findById(1L);
+			log.info(customer.toString());
+
+			log.info("findByLastName---------------------------");
+			repository.findByLastName("aa").forEach(c -> {log.info(c.toString());});
+			log.info("");
+		};
 	}
 }
